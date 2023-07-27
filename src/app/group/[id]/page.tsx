86a498b9bb28ff149db80fb212cf64df1fbb0696
Carlos Venegas <cvj_2311@hotmail.com>
2023-07-27 02:46:58 -0500

    commented incomplete files

--- src/app/group/[id]/page.tsx
+++ src/app/group/[id]/page.tsx
@@ -1,6 +1,6 @@
-export default function Group({ params }) {
-    const {id} = params;
-    return (
-        <h1>Group {id}</h1>
-    );
-}
+// export default function Group({ params }) {
+//     const {id} = params;
+//     return (
+//         <h1>Group {id}</h1>
+//     );
+// }
